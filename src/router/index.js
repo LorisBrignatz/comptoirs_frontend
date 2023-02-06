629063541d007a8e1b74453d49d94a44a6d35c7a
Loris019 <loris.brignatz@etud.univ-jfc.fr>
2023-02-06 10:41:06 +0000

    Ajout du fichier ProduitsView

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CategorieView from '../views/CategorieView.vue'
+import ProduitsView from '../views/ProduitsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
